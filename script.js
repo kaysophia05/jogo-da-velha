6b641dba8174553d2f6d883a48bbe146194edf50
kaylane sophia <kaylanesophia220@gmail.com>
2025-02-11 21:21:54 -0300

    reiniciar o jogo

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const cellElements = document.querySelectorAll('[data-cell]');
 const board = document.querySelector("[data-board]");
 const winningMessageTextElement = document.querySelector("[data-winning-message-text]");
 const winningMessage = document.querySelector("[data-winning-message]");
+const restartButton = document.querySelector("[restart-button]");
 let isCircleTurn;
 const winningCombinations = [
     [0, 1, 2],
@@ -13,25 +14,39 @@ const winningCombinations = [
     [0, 4, 8],
     [2, 4, 6],
 ];
+
 //Função para dar inicio ao jogo, aplicando um classa a boardpara a primeira jogada de símbolo
 const startgame = () =>{
-    for(const cell of cellElements){    
+    isCircleTurn = false;
+    for(const cell of cellElements){   
+        cell.classList.remove("x");
+        cell.classList.remove("circle");
+        cell.removeEventListener("click",handleClick)
         cell.addEventListener("click", handleClick,{once:true});
     }
-    isCircleTurn = false;
     //O jogo já inicia com hover do símbolo "X"
-    board.classList.add("x")
+    setBoardHoverClass();
+    winningMessage.classList.remove("show-winning-message");
 }
 
-const placeMark = (cell, classToAdd) => {
-    cell.classList.add(classToAdd);
-};
+//Função para aplicação da lógica do hover pela board
+setBoardHoverClass = () =>{
+    //Removendo a classe do simbolo que estiver no momento, para evitar o acúmulo
+    board.classList.remove("x");
+    board.classList.remove("circle");
+    //Adicionado a classe do símbolo da vez na board, para a adição correta do hover ao mudar a vez da jogada
+    if(isCircleTurn){
+        board.classList.add("circle")
+    } else{
+        board.classList.add("x");
+    }
+}
 
 const endGame = (isDraw) => {
     if (isDraw) {
       winningMessageTextElement.innerText = "Empate!";
     } else {
-      winningMessageTextElement.innerText = isCircleTurn
+        winningMessageTextElement.innerText = isCircleTurn
         ? "O Venceu!"
         : "X Venceu!";
     }
@@ -48,18 +63,14 @@ const checkForWin = (currentPlayer) =>{
     });
 };
 
+const placeMark = (cell, classToAdd) => {
+    cell.classList.add(classToAdd);
+};
+
 const swapTurn = () =>{
     //isCircleTurn recebe a negação do seu valor para que a mudança de símbolo posssa ser realizada
     isCircleTurn = !isCircleTurn;
-    //Removendo a classe do simbolo que estiver no momento, para evitar o acúmulo
-    board.classList.remove("x");
-    board.classList.remove("circle");
-    //Adicionado a classe do símbolo da vez na board, para a adição correta do hover ao mudar a vez da jogada
-    if(isCircleTurn){
-        board.classList.add("circle")
-    } else{
-        board.classList.add("x");
-    }
+    setBoardHoverClass();
 }
 
 const handleClick = (e) =>{
@@ -79,6 +90,7 @@ const handleClick = (e) =>{
 }
 
 startgame();
+restartButton.addEventListener("click",startgame)
 
 /*
     --Etapas da função CheckForWin--
@@ -87,3 +99,12 @@ startgame();
     every: acessa uma combinação e verifica se cada index daquela combinação contem o jogador do momento
     contains: Verifica pelo index se sua classe é o jogador do momento, sendo 'X' ou 'circle'
 */
+
+/*
+    --Função setBoardHoverClass: Explicação
+    Se o jogador x vencer uma rodada e o botão de reiniciar for clicado a vez "passa" a ser do circulo, 
+    todavia, ocorre um conflito, pois definimos que o jogo ja se inicia com o "x"
+    para evitar o bug no hover do simbolos, criamos a função com a lógica de retirar as classes da board 
+    e adicionar conforme a validade do isCircleTurn (que na fnção de startgame, já iniciamos como falso)
+
+*/ 
